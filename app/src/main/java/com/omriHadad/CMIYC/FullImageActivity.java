204ea4dbfcf475fb4f627beb42e207fe67dede5a
yosikadosh <kadosh01@gmail.com>
2018-10-21 10:43:48 +0300

    20.10 23:00

--- app/src/main/java/com/omriHadad/CMIYC/FullImageActivity.java
+++ app/src/main/java/com/omriHadad/CMIYC/FullImageActivity.java
@@ -1,9 +1,11 @@
 package com.omriHadad.CMIYC;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
@@ -15,6 +17,7 @@ public class FullImageActivity extends AppCompatActivity
 {
     RelativeLayout top_bar;
     RelativeLayout buttom_bar;
+    File image;
     boolean hide;
 
     @Override
@@ -29,8 +32,8 @@ public class FullImageActivity extends AppCompatActivity
         int position = i.getExtras().getInt("id");
         ImageAdapter adapter = new ImageAdapter(this);
         ImageView imageView = findViewById(R.id.image);
-        File a =adapter.images.get(position);
-        Bitmap myBitmap = BitmapFactory.decodeFile(a.getAbsolutePath());
+        image =adapter.images.get(position);
+        Bitmap myBitmap = BitmapFactory.decodeFile(image.getAbsolutePath());
         //imageView.setImageURI(Uri.fromFile(a));
         imageView.setImageBitmap(myBitmap);
         imageView.setScaleType(ImageView.ScaleType.FIT_XY);
@@ -41,7 +44,34 @@ public class FullImageActivity extends AppCompatActivity
     }
 
     public void delete_buttonOnClick(View v){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Are you sure you want to delete the image ?");
+        alert.setMessage("");
+        alert.setCancelable(false);
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                startActivity(new Intent(FullImageActivity.this, ImageGallery.class));
+            }
+        });
+        alert.setNegativeButton("No",new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
 
+            }
+        });
+        alert.create().show();
+        if (image.exists()) {
+            if (image.delete()) {
+                System.out.println("file Deleted :" + image.getName());
+            } else {
+                System.out.println("file not Deleted :" + image.getName());
+            }
+        }
     }
     public void imageOnClick(View v){
         if(hide){
